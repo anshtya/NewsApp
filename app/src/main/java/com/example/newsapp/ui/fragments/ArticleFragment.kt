c34e4e9ca7d94cfe2e9e56342f2e4f4b5fd63e23
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-12-20 22:13:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,18 @@
+package com.example.newsapp.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.newsapp.R
+import com.example.newsapp.ui.NewsActivity
+import com.example.newsapp.ui.NewsViewModel
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+    private val viewModel: NewsViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
