eee86ccc568bc031d0394d77ecdc399699d6541d
Ansh Tyagi <tyagiansh486@gmail.com>
2022-12-29 14:45:37 +0530

    code fixes

--- app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
@@ -35,11 +35,37 @@ class NewsViewModel(
     }
 
     fun getBreakingNews(countryCode: String) = viewModelScope.launch {
-        safeBreakingNewsCall(countryCode)
+        breakingNews.postValue(Resource.Loading())
+        try {
+            if(hasInternetConnection()){
+                val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+                breakingNews.postValue(handleBreakNewsResponse(response))
+            } else {
+                breakingNews.postValue(Resource.Error("No Internet Connection"))
+            }
+        } catch (t: Throwable){
+            when(t){
+                is IOException -> breakingNews.postValue(Resource.Error("Network Failure"))
+                else -> breakingNews.postValue(Resource.Error("Conversion Error"))
+            }
+        }
     }
 
     fun searchNews(searchQuery: String) = viewModelScope.launch {
-        safeSearchNewsCall(searchQuery)
+        searchNews.postValue(Resource.Loading())
+        try {
+            if(hasInternetConnection()){
+                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+                searchNews.postValue(handleSearchNewsResponse(response))
+            } else {
+                searchNews.postValue(Resource.Error("No Internet Connection"))
+            }
+        } catch (t: Throwable){
+            when(t){
+                is IOException -> searchNews.postValue(Resource.Error("Network Failure"))
+                else -> searchNews.postValue(Resource.Error("Conversion Error"))
+            }
+        }
     }
 
     private fun handleBreakNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
@@ -87,40 +113,6 @@ class NewsViewModel(
     fun deleteArticle(article: Article) {
         viewModelScope.launch {
             newsRepository.delete(article)
-        }
-    }
-
-    private suspend fun safeBreakingNewsCall(countryCode: String){
-        breakingNews.postValue(Resource.Loading())
-        try {
-            if(hasInternetConnection()){
-                val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
-                breakingNews.postValue(handleBreakNewsResponse(response))
-            } else {
-                breakingNews.postValue(Resource.Error("No Internet Connection"))
-            }
-        } catch (t: Throwable){
-            when(t){
-                is IOException -> breakingNews.postValue(Resource.Error("Network Failure"))
-                else -> breakingNews.postValue(Resource.Error("Conversion Error"))
-            }
-        }
-    }
-
-    private suspend fun safeSearchNewsCall(searchQuery: String){
-        searchNews.postValue(Resource.Loading())
-        try {
-            if(hasInternetConnection()){
-                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
-                searchNews.postValue(handleSearchNewsResponse(response))
-            } else {
-                searchNews.postValue(Resource.Error("No Internet Connection"))
-            }
-        } catch (t: Throwable){
-            when(t){
-                is IOException -> searchNews.postValue(Resource.Error("Network Failure"))
-                else -> searchNews.postValue(Resource.Error("Conversion Error"))
-            }
         }
     }
 
