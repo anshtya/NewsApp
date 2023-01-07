213f424eb1bd15c4ab0f5ba6e7bbd00bceca0ed9
Ansh Tyagi <tyagiansh486@gmail.com>
2023-01-07 18:58:07 +0530

    Update Constants.kt

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -2,7 +2,6 @@ package com.example.newsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "your_api_key"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
