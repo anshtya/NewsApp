376b16ac0062c9f6b28c4ceb022e8632d22d92bf
Ansh Tyagi <tyagiansh486@gmail.com>
2023-02-11 12:08:43 +0530

    removed unused variable

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -3,7 +3,6 @@ package com.example.newsapp.util
 class Constants {
     companion object{
         const val BASE_URL = "https://newsapi.org"
-        const val SEARCH_NEWS_TIME_DELAY = 500L
         const val COUNTRY_CODE = "in"
         const val QUERY_PAGE_SIZE = 20
     }
